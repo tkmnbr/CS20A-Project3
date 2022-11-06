7cfbe7b6d56f554d339c5b54bb228a0535cf70dc
Noboru Tokimi <t.nbr0304@gmail.com>
2022-11-06 00:55:26 -0700

    Add project files.

---
+++ CS20A Project3/list.h
@@ -0,0 +1,424 @@
+#ifndef F22_CS20A_LIST_H
+#define F22_CS20A_LIST_H
+
+#include<iostream>
+#include<assert.h>
+// Linked List object that maintains both m_front and m_rear pointers
+// and the m_size of the list.  Note that you have to keep the m_front,
+// m_rear and m_size consistent with the intended state of the List 
+// otherwise very bad things happen. 
+template<typename Type>
+class List {
+public:
+
+	List();
+
+	List(const List<Type>& other);
+
+	List<Type>& operator=(const List<Type>& other);
+
+	~List();
+
+	void	print() const;
+	bool	empty() const;
+
+	void	push_front(const Type &value);
+	void	push_rear(const Type &value);
+	void	push_at(int idx, const Type &value);
+
+	// Note that the user must m_front ensure the list is not empty
+	// prior to calling these functions. 
+	Type	front() const;
+	Type	rear() const;
+	Type	at(int idx) const;
+
+	int		size() const;
+	int		count(const Type &value) const;
+
+	int		find(const Type &value) const;
+
+	bool	pop_front();
+	bool	pop_rear();
+	bool	pop_at(int idx);
+	int		pop_value(const Type &value);
+
+#ifndef MAKE_MEMBERS_PUBLIC
+private:
+#endif
+	// Forward declare the nodes for our List.
+	// Will be implemented along with list
+	// member functions
+	class DLNode;
+
+	// We'll have both m_front and m_rear points for 
+	// Fast insertion/deletion from both ends.
+	DLNode*	m_front;
+	DLNode*	m_rear;
+
+	// Keep track of number of nodes in the list
+	int		m_size;
+};
+
+
+/* List Implementation
+//
+//  Since List is a template class (which is not an actual
+//  class yet, not until we actually instantiate the list)
+//  we need to keep the implementation together with
+//  the definition.  There are ways to simulate having
+//  separate "implementation/definition" with templates,
+//  but they aren't necessary and can be confusing.
+*/
+
+/* DLNode definition
+//		Already implemented, nothing to do here but to use it.
+//		Make note of the fact the the member variables are private
+*/
+template<typename Type>
+class List<Type>::DLNode {
+public:
+	DLNode() :nxt(nullptr), prv(nullptr) {}
+	DLNode(Type v, DLNode* p, DLNode* n) : val(v), nxt(n), prv(p) {}
+
+	DLNode * next() const { return nxt; } // Access the address of the next node
+	void	 next(DLNode *n) { nxt = n; } // Modify the address of the next node
+
+	DLNode * prev() const { return prv; } // Access the address of the previous node
+	void	 prev(DLNode *p) { prv = p; } // Modify the address of the previous node
+
+	Type value() const { return val; }		// Access the value stored in the current node
+	void value(const Type &v) { val = v; }	// Modify the value stored in the current node
+
+private:
+	Type  val;
+	DLNode* nxt;
+	DLNode* prv;
+};
+
+
+
+/* List default constructor
+//		Set m_front and m_rear pointer to point to nothing, m_size is zero
+//		Already implemented, nothing to do.
+*/
+template<typename Type>
+List<Type>::List() :m_front(nullptr), m_rear(nullptr), m_size(0) {
+}
+
+
+/* Copy constructor
+*/
+template<typename Type>
+List<Type>::List(const List<Type>& other) {
+
+
+	/*   TODO   */
+
+}
+/* Overloaded assignment operator
+*/
+template<typename Type>
+List<Type>& List<Type>::operator=(const List<Type>& other) {
+
+
+	/*   TODO   */
+
+	return *this;
+}
+
+
+/* List destructor
+*/
+template<typename Type>
+List<Type>::~List() {
+
+
+	/*   TODO   */
+}
+
+/* List print
+*/
+template<typename Type>
+void List<Type>::print() const {
+	DLNode* curr = m_front;
+	while (curr != nullptr) {
+		std::cout << curr->value();
+		curr = curr->next();
+	}
+}
+
+/* List empty
+*/
+template<typename Type>
+bool List<Type>::empty() const {
+
+
+	/*   TODO   */
+	bool retval = false;
+	return retval;
+
+}
+
+
+/* List push_front
+*/
+template<typename Type>
+void List<Type>::push_front(const Type &value) {
+	DLNode* node = new DLNode;
+	if (m_front == nullptr)
+		m_rear = node;
+	node->value(value);
+	node->prev(nullptr);
+	node->next(m_front);
+	m_front = node;
+	m_size++;
+}
+
+/* List push_rear
+*/
+template<typename Type>
+void List<Type>::push_rear(const Type &value) {
+
+
+	/*   TODO   */
+}
+
+/* List push_at
+*/
+template<typename Type>
+void List<Type>::push_at(int idx, const Type &value) {
+
+
+	/*   TODO   */
+}
+
+/*  List front
+*/
+template<typename Type>
+Type List<Type>::front() const {
+	// Force check that the m_front is pointing to a DLNode
+	// Typical solution for deployement code is to throw
+	// exceptions, but since we haven't covered that yet
+	// we'll make due with assert, which is used for testing
+	assert(m_front != nullptr);
+
+
+
+	/*   TODO   */
+
+	Type retval;
+	return retval;
+}
+
+/* List rear
+*/
+template<typename Type>
+Type List<Type>::rear() const {
+
+	// Force check that the m_rear is pointing to a DLNode
+	// Typical solution for deployement code is to throw
+	// exceptions, since we haven't covered that yet
+	// we'll make due with assert, which is used for testing
+	assert(m_rear != nullptr);
+
+	/*   TODO   */
+
+	Type retval;
+	return retval;
+
+}
+
+/* List at
+*/
+template<typename Type>
+Type List<Type>::at(int idx) const {
+
+	// Force index to be correct before getting the Type
+	// Typical solution for deployement code is to throw
+	// exceptions, since we haven't covered that yet
+	// we'll make due with assert, which is used for testing
+	assert(idx >= 0 && idx < m_size);
+
+	/*   TODO   */
+
+	Type retval;
+	return retval;
+}
+
+/* List size
+//		return the m_size of the list
+*/
+template<typename Type>
+int List<Type>::size() const {
+
+
+	/*   TODO   */
+
+	int retval = 9000000;
+	return retval;
+}
+template<typename Type>
+int List<Type>::count(const Type &value) const {
+
+
+	/*   TODO   */
+
+	int retval = 9000000;
+	return retval;
+
+}
+/* List find
+*/
+template<typename Type>
+int List<Type>::find(const Type &value) const {
+
+	/*   TODO   */
+
+
+	int retval = 8675309;
+	return retval;
+
+}
+
+/* List pop_front
+*/
+template<typename Type>
+bool List<Type>::pop_front() {
+
+	/*   TODO   */
+
+	bool retval = false;
+	return retval;
+}
+
+/* List pop_rear
+*/
+template<typename Type>
+bool List<Type>::pop_rear() {
+
+
+	/*   TODO   */
+
+	bool retval = false;
+	return retval;
+}
+
+/* List pop_at
+*/
+template<typename Type>
+bool List<Type>::pop_at(int idx) {
+
+
+	/*   TODO   */
+
+	bool retval = false;
+	return retval;
+
+}
+
+/* List pop_at on value
+*/
+template<typename Type>
+int List<Type>::pop_value(const Type &value) {
+
+	/*   TODO   */
+
+	int retval = 42;
+	return retval;
+
+}
+
+
+#endif
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// _X_XMMXXI
---
+++ CS20A Project3/main.cpp
@@ -0,0 +1,182 @@
+
+
+
+// Choose which run to compile
+#define RUN0 //RUN1 RUN2 RUN3 
+
+
+#ifdef RUN0 
+
+#include"list.h"
+#include"studentinfo.h"
+#include<iostream>
+
+int main() {
+	std::cout << StudentInfo::name() << std::endl;
+	std::cout << StudentInfo::id() << std::endl;
+
+	List<int> * l = new List<int>;; //List of integers
+
+	l->push_front(5);
+	l->push_front(4);
+	l->push_front(3);
+	l->push_front(2);
+
+	/*l->push_rear(1);
+
+	l->push_front(5);
+
+	l->push_front(7);
+
+	l->push_front(42);*/
+
+	l->print();
+
+
+	return 0;
+
+}
+
+#elif defined RUN1
+
+#include"list.h"
+#include"studentinfo.h"
+#include<iostream>
+#include<string>
+
+int main() {
+
+	std::cout << StudentInfo::name() << std::endl;
+	std::cout << StudentInfo::id() << std::endl;
+
+	List<std::string> * l = new List<std::string>; //List of integers
+
+	l->push_rear("Hello");
+	l->push_front("World");
+	l->push_front("Bob");
+	l->push_front("2");
+
+	l->print(); // 2 Bob World Hello
+
+	l->pop_rear();  // Will trip assert if empty
+	l->print(); // 2 Bob World 
+
+	std::cout << l->front() << std::endl; // 2
+
+	return 0;
+
+}
+
+#elif defined RUN2
+
+#define MAKE_MEMBERS_PUBLIC
+#include"list.h"
+#include<assert.h>
+
+//Unit test example for push_font on initially empty list
+
+int main() {
+
+	// Step 1: Initial state
+	// Assume that constructor implemented properly
+	// If you're not sure if the constructor is 
+	// implemented properly, STOP, go back and make
+	// sure. Luckily the default constructor is 
+	// done for you.  But the point remains, never,
+	// ever, use a function in a test that isn't itself
+	// verified to function according to spec.
+	List<int> * l = new List<int>;
+
+
+	// Step 2: Execute the function to test
+	l->push_front(99);
+
+	// Step 3: Verify what you expect to observe.
+	// You must know what is supposed to happen in
+	// order to analyze for correctness. Generally, 
+	// you must permute all possible combinations
+	// of all member variables.
+	assert(l->m_front != nullptr);
+	assert(l->m_rear != nullptr);
+	assert(l->m_size == 1);
+	assert(l->m_front->prev() == nullptr);
+	assert(l->m_front->next() == nullptr);
+	assert(l->m_front->value() == 99);
+
+	return 0;
+}
+
+#elif defined RUN3
+
+#define MAKE_MEMBERS_PUBLIC
+#include"list.h"
+#include<assert.h>
+#include<string>
+
+//Unit test example for push_font on initially non empty list
+
+int main() {
+
+	// Step 1: Initial state
+	// Same as above, except we must m_front populate the list
+	// However, since we are testing push_front, we cannot
+	// use it to generate the initial state of our list
+	// to test.  So we have to manually set the initial state
+	// in this case we are creating a list with three nodes.
+	List<std::string>::DLNode* m_front = new List<std::string>::DLNode;
+	List<std::string>::DLNode* second = new List<std::string>::DLNode;
+	List<std::string>::DLNode* third = new List<std::string>::DLNode;
+
+	// We have to manually linked everything together:
+	m_front->value("first");
+	m_front->prev(nullptr);
+	m_front->next(second);
+
+	second->value("second");
+	second->prev(m_front);
+	second->next(third);
+
+	third->value("third");
+	third->prev(second);
+	third->next(nullptr);
+
+	// Now we create  List object and inject the initial control state:
+	List<std::string> * l = new List<std::string>;
+	l->m_front = m_front;
+	l->m_rear = third;
+	l->m_size = 3;
+
+	// Step 2: Now with the list given a complete and correct starting state
+	// We can test various member functions for correctness.
+	// For example we can use this as one test for push_front;
+	l->push_front("Zero");
+
+	// Step 3: Verify:  Technically, we should examine all 
+	// member variables, and all nodes' value, nxt, prv pointers
+	// to ensure that the push_front didn't mangle any of the
+	// internal nodes.  But, this is mostly just to illustrate
+	// how you might develop your own tests.  
+	assert(l->m_size == 4);
+	assert(l->m_front->prev() == nullptr);
+	assert(l->m_front == m_front->prev());
+	assert(l->m_front->next() == m_front);
+	assert(l->m_front->value() == "Zero");
+	assert(l->m_rear == third);
+
+	return 0;
+}
+
+#else
+
+
+// You will have to count the tests when you go through them yourself
+// You can post them on the disucssion forum to help our your classmates
+// and earn participcation
+#include"listtests_f22.h"
+int main() {
+	runTest(test_empty, 0);
+	
+	return 0;
+}
+
+#endif
